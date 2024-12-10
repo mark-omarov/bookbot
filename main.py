78f7b7919142f73f4f5e74089f091e11b3502037
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2024-12-10 12:57:54 +0900

    build simple bookbot

---
+++ main.py
@@ -0,0 +1,46 @@
+def main():
+    book_path = "./books/frankenstein.txt"
+    text = get_text(book_path)
+    words_count = count_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_dict_list = get_chars_dict_list(chars_dict)
+    chars_dict_list.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{words_count} words found in the document")
+    for item in chars_dict_list:
+        print(f"The '{item["name"]}' character was found {item["num"]} times")
+    print("--- End report ---")
+
+
+def get_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def count_words(text):
+    return len(text.split())
+
+
+def get_chars_dict(text):
+    chars_dict = {}
+    for char in text:
+        lowered = char.lower()
+        if char not in chars_dict:
+            chars_dict[lowered] = 1
+        else:
+            chars_dict[lowered] += 1
+    return chars_dict
+
+
+def get_chars_dict_list(chars_dict):
+    chars_dict_list = []
+    for key in chars_dict:
+        chars_dict_list.append({"name": key, "num": chars_dict[key]})
+    return chars_dict_list
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+main()
